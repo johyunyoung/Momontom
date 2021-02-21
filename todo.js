f1ae725a261fe0bc5db094ef4e183b45db1b2d32
lucky7jhy <lucky7jhy@gmail.com>
2021-02-21 23:06:11 +0900

    done

---
+++ todo.js
@@ -0,0 +1,140 @@
+const todoForm = document.querySelector(".js-todo-form");
+const todoInput = todoForm.querySelector("input");
+const pendingList = document.querySelector(".js-pending");
+const finishedList = document.querySelector(".js-finished");
+
+const PENDING_LS = "pending";
+const FINISHED_LS = "finish";
+
+let PENDINGS = [];
+let FINISHEDS = [];
+let idNum = 0;
+
+function savePendings(){
+    localStorage.setItem(PENDING_LS, JSON.stringify(PENDINGS));
+}
+
+function saveFinisheds(){
+    localStorage.setItem(FINISHED_LS, JSON.stringify(FINISHEDS));
+}
+
+function handleFinDel(event){
+    const li = event.target.parentNode;
+    finishedList.removeChild(li);
+    const cleanFinisheds = FINISHEDS.filter(function(finished){
+        return finished.id !== parseInt(li.id);
+    })
+    FINISHEDS = cleanFinisheds;
+    saveFinisheds();
+}
+
+function handleFinBack(event){
+    const li = event.target.parentNode;
+    //move to pen and del
+    const text = li.querySelector("span").innerText;
+    paintPending(text);
+    handleFinDel(event);
+}
+
+function handlePendingDel(event){
+    const li = event.target.parentNode;
+    pendingList.removeChild(li);
+    const cleanPendings = PENDINGS.filter(function(pending){
+        return pending.id !== parseInt(li.id);
+    })
+    PENDINGS = cleanPendings;
+    savePendings();
+}
+
+function handlePendingFin(event){
+    const li = event.target.parentNode;
+    // move to fin and del
+    const text = li.querySelector("span").innerText;
+    paintFinished(text);
+    handlePendingDel(event);
+}
+
+function paintFinished(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const backBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = idNum;     // bug
+    idNum += 1;
+    span.innerText = text;
+    delBtn.innerText = "❌";
+    backBtn.innerText = "⏪";
+    delBtn.addEventListener("click", handleFinDel);
+    backBtn.addEventListener("click", handleFinBack);
+    li.appendChild(delBtn);
+    li.appendChild(backBtn);
+    li.appendChild(span);
+    li.id = newId;
+    finishedList.appendChild(li);
+    const finishedObj = {
+        text: text,
+        id: newId
+    };
+    FINISHEDS.push(finishedObj);
+    saveFinisheds();
+}
+
+function paintPending(text){
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const finBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = idNum;      // bug
+    idNum += 1;
+    span.innerText = text;
+    delBtn.innerText = "❌";
+    finBtn.innerText = "✔";
+    delBtn.addEventListener("click", handlePendingDel);
+    finBtn.addEventListener("click", handlePendingFin);
+    li.appendChild(delBtn);
+    li.appendChild(finBtn);
+    li.appendChild(span);
+    li.id = newId;
+    pendingList.appendChild(li);
+    const pendingObj = {
+        text: text,
+        id: newId
+    };
+    PENDINGS.push(pendingObj);
+    savePendings();
+}
+
+function loadPending(){
+    const loadedPendings = localStorage.getItem(PENDING_LS);
+    if(loadedPendings !== null){
+        const parsedPendings = JSON.parse(loadedPendings);
+        parsedPendings.forEach(function(pending){
+            paintPending(pending.text);
+        })
+    }
+}
+
+function loadFinished(){
+    const loadedFinisheds = localStorage.getItem(FINISHED_LS);
+    if(loadedFinisheds !== null){
+        const parsedFinisheds = JSON.parse(loadedFinisheds);
+        parsedFinisheds.forEach(function(finished){
+            paintFinished(finished.text);
+        })
+    }
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const inputText = todoInput.value;
+    paintPending(inputText);
+    todoInput.value = "";
+}
+
+function init() {
+    loadPending();
+    loadFinished();
+    todoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
---
+++ weather.js
@@ -0,0 +1,55 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY = "e33354e342a79d6ce51f8415d812bf8c";
+const COORDS = "coords";
+
+function getWeather(lat, lng){
+    console.log("get");
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response){
+        return response.json()
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature}° @ ${place}`;
+    })
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoError(){
+    console.log("error");
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
